bd24faea1d5ab7a06e750bf9b941195f76f3ebc4
MaomiHz <maomihz@gmail.com>
2018-03-08 11:43:39 -0600

    Initial Commit

---
+++ SafeBuffer.cpp
@@ -0,0 +1,31 @@
+//
+//  SafeBuffer.cpp
+//
+//
+//  Created by Joshua Higginbotham on 11/4/15.
+//
+//
+
+#include "SafeBuffer.h"
+#include <string>
+#include <queue>
+
+SafeBuffer::SafeBuffer() {
+	
+}
+
+SafeBuffer::~SafeBuffer() {
+	
+}
+
+int SafeBuffer::size() {
+    return 0;
+}
+
+void SafeBuffer::push_back(std::string str) {
+	return;
+}
+
+std::string SafeBuffer::retrieve_front() {
+	return "";
+}
