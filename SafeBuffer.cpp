e9d19a3664cf606c76b9c0ed83b46e016d2c7dd9
MaomiHz <maomihz@gmail.com>
2018-03-11 11:33:53 -0500

    bworks

--- SafeBuffer.cpp
+++ SafeBuffer.cpp
@@ -9,23 +9,74 @@
 #include "SafeBuffer.h"
 #include <string>
 #include <queue>
+#include <utility>
 
-SafeBuffer::SafeBuffer() {
-	
+SafeBuffer::SafeBuffer() : vec() {
+  pthread_mutex_init(&mtx, NULL);
 }
 
 SafeBuffer::~SafeBuffer() {
-	
+  pthread_mutex_destroy(&mtx);
 }
 
 int SafeBuffer::size() {
-    return 0;
+    return vec.size();
 }
 
 void SafeBuffer::push_back(std::string str) {
-	return;
+  pthread_mutex_lock(&mtx);
+  vec.push_back(str);
+  pthread_mutex_unlock(&mtx);
+  return;
 }
 
 std::string SafeBuffer::retrieve_front() {
-	return "";
+  pthread_mutex_lock(&mtx);
+  std::string s = vec.front();
+  vec.pop_front();
+  pthread_mutex_unlock(&mtx);
+  return s;
 }
+
+
+SafeCounter::SafeCounter() : cmap() {
+  pthread_mutex_init(&mtx, NULL);
+}
+
+SafeCounter::~SafeCounter() {
+  pthread_mutex_destroy(&mtx);
+}
+
+void SafeCounter::inc(std::string s) {
+  pthread_mutex_lock(&mtx);
+  try {
+    cmap.at(s) ++;
+    pthread_mutex_unlock(&mtx);
+  } catch (std::out_of_range e) {
+    cmap[s] = 1;
+    pthread_mutex_unlock(&mtx);
+  }
+}
+
+void SafeCounter::inc(std::string s, int num) {
+  inc(transform(s, num));
+}
+
+unsigned int SafeCounter::count(std::string s) {
+  try {
+    return cmap.at(s);
+  } catch (std::out_of_range e) {
+    return 0;
+  }
+}
+
+unsigned int SafeCounter::count(std::string s, int num) {
+  return count(transform(s, num));
+}
+
+std::string SafeCounter::transform(std::string s, int num) {
+  return s + "$$" + std::to_string(num);
+}
+
+
+// unsigned int
--- SafeBuffer.h
+++ SafeBuffer.h
@@ -1,6 +1,6 @@
 //
 //  SafeBuffer.h
-//  
+//
 //
 //  Created by Joshua Higginbotham on 11/4/15.
 //
@@ -12,25 +12,50 @@
 #include <stdio.h>
 #include <queue>
 #include <string>
+#include <list>
 #include <pthread.h>
+#include <unordered_map>
 
 class SafeBuffer {
-	/*
-		Only two data members are really necessary for this class:
-		a mutex, and a data structure to hold elements. Recall
-		that SafeBuffer is supposed to be FIFO, so something
-		like std::vector will adversely affect performance if
-		used here. We recommend something like std::list
-		or std::queue, because std::vector is very inefficient when
-		being modified from the front.
-	*/
-	
+  /*
+    Only two data members are really necessary for this class:
+    a mutex, and a data structure to hold elements. Recall
+    that SafeBuffer is supposed to be FIFO, so something
+    like std::vector will adversely affect performance if
+    used here. We recommend something like std::list
+    or std::queue, because std::vector is very inefficient when
+    being modified from the front.
+  */
+
 public:
-    SafeBuffer();
-	~SafeBuffer();
-	int size();
-    void push_back(std::string str);
-    std::string retrieve_front();
+  SafeBuffer();
+  ~SafeBuffer();
+  int size();
+  void push_back(std::string str);
+  std::string retrieve_front();
+private:
+	pthread_mutex_t mtx;
+  std::list<std::string> vec;
+};
+
+
+class SafeCounter {
+  /*
+    SafeCounter is a wrapper around unordered_list. Counts frequency of
+    a string.
+  */
+
+public:
+  SafeCounter();
+  ~SafeCounter();
+  void inc(std::string s);
+  void inc(std::string s, int num);
+  unsigned int count(std::string s);
+  unsigned int count(std::string s, int num);
+private:
+  pthread_mutex_t mtx;
+  std::unordered_map<std::string, unsigned int> cmap;
+  std::string transform(std::string s, int num);
 };
 
 #endif /* SafeBuffer_ */
